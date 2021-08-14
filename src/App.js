41b694b57bb382b89c7055099cd34745c6580ff0
caarlosdamian <dapecarlos@gmail.com>
2021-08-14 12:56:12 -0500

    finish login and styled

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import React, { useState } from "react";
+import LoginForm from "./components/LoginForm";
+
 function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+    if (
+      details.email === adminUser.email &&
+      details.password === adminUser.password
+    ) {
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      setError("Details do not match");
+    }
+  };
+  const Logout = () => {
+    setUser({ name: "", email: "" });
+  };
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {user.email !== "" ? (
+        <div className="welcome">
+          <h2>
+            Welcome, <span>{user.name}</span>
+          </h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
